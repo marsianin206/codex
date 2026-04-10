515bca16da17256c9a22c4a9786e23ce6471bca4
X.FEAR <75545586+marsianin206@users.noreply.github.com>
2026-04-11 02:50:34 +0300

    Fix typo: provider -> providers

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -2,7 +2,7 @@ import NextAuth from "next-auth";
 import GitHub from "next-auth/providers/github";
 
 const handler = NextAuth({
-  provider: [
+  providers: [
     GitHub({
       clientId: process.env.GITHUB_CLIENT_ID,
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
